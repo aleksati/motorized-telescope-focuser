7467df2a0dc9f35089d92909f3c9a3312cad764e
AleksanderTidemann <aleksandertid@gmail.com>
2021-08-09 18:20:53 +0200

    Update

--- fov-calculator/src/components/forminfo.jsx
+++ fov-calculator/src/components/forminfo.jsx
@@ -2,31 +2,28 @@ import React from "react";
 
 const FormInfo = (props) => (
   <div className="form-group border border-white rounded m-2">
-    <h2 className="ml-2 mt-2 text-light">{props.title}</h2>
     <div className="d-flex">
-      {props.items.map((item) => {
-        return (
-          <div className="form-label-group" key={item.ref}>
-            <input
-              type={item.type}
-              placeholder={item.name}
-              id={item.ref}
-              name={item.ref}
-              onChange={props.onChange}
-              value={item.value}
-              required={item.required}
-              className="form-control ml-2"
-              aria-describedby="addon"
-            />
-            <label htmlFor={item.ref}>{item.name}</label>
-            <div className="input-group-append">
-              <span className={props.addoncolor} id={item.ref}>
-                {item.unit}
-              </span>
-            </div>
-          </div>
-        );
-      })}
+      return (
+      <div className="form-label-group">
+        <input
+          type={item.type}
+          placeholder={item.name}
+          id={item.ref}
+          name={item.ref}
+          onChange={props.onChange}
+          value={item.value}
+          required={item.required}
+          className="form-control ml-2"
+          aria-describedby="addon"
+        />
+        <label htmlFor={item.ref}>{item.name}</label>
+        <div className="input-group-append">
+          <span className={props.addoncolor} id={item.ref}>
+            {item.unit}
+          </span>
+        </div>
+      </div>
+      );
     </div>
   </div>
 );
