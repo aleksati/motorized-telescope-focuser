198264d0101a8d00db6b9bba4918990427a7e07a
AleksanderTidemann <aleksandertid@gmail.com>
2021-12-04 17:48:59 +0100

    Style update

--- fov-calculator/src/index.js
+++ fov-calculator/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./css/App.css";
-import "./css/floating-label.css";
+import "./css/custom-style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
